f2945cad753c7570a19cc11bcb1cd152e94ffb1b
pratap bunga <pratap234337@gmail.com>
2018-11-26 21:05:51 +0530

    Updates

---
+++ precache-manifest.12e36b63a03ab65dccfb5b7912255126.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bfbaf710c8510dc50a5",
+    "url": "pratap234337.github.io/contact-manager/static/js/runtime~main.9bfbaf71.js"
+  },
+  {
+    "revision": "ffc7919915682e82a413",
+    "url": "pratap234337.github.io/contact-manager/static/js/main.ffc79199.chunk.js"
+  },
+  {
+    "revision": "e6120689bafc5854afbd",
+    "url": "pratap234337.github.io/contact-manager/static/js/1.e6120689.chunk.js"
+  },
+  {
+    "revision": "ffc7919915682e82a413",
+    "url": "pratap234337.github.io/contact-manager/static/css/main.a96dbbe4.chunk.css"
+  },
+  {
+    "revision": "4c77a781f39ccec18f80422216fb73b7",
+    "url": "pratap234337.github.io/contact-manager/index.html"
+  }
+];
